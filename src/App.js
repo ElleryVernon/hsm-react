adf8314127860b835621ee295c7d864c35125560
SeongMin Han <seongminhan@MacBook-Pro-4.local>
2023-08-26 15:58:12 +0900

    my first react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          성민의 리액트 애플리케이션
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
